113391bf5c9e91bfa57773bf86520c2f5cecbd2d
unknown <williampo78@gmail.com>
2022-02-07 16:32:21 +0800

    show no favCards message

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -71,7 +71,7 @@ export default {
       });
       if (!this.cards.length) {
         this.$emit("noCards");
-      } else if (!favCards.length) {
+      } else if (!this.cards.length || !favCards.length) {
         this.$emit("noFav");
       }
       console.log("snapshot");
